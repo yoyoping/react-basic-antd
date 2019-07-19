6be7802e2082385b1667b9fcfc089dbb3cdae341
yoyoping <zhangping_ymd@outlook.com>
2019-07-19 17:21:19 +0800

    增加懒加载，优化代码

--- src/route/Route.ts
+++ src/route/Route.ts
@@ -1,27 +1,32 @@
-/**
- * 路由集中整理，这里的只是显示在Layout组件里面的路由，通过登录、权限动态生成路由
+/*
+ * @Description: 路由集中整理，这里的只是显示在Layout组件里面的路由，通过登录、权限动态生成路由
+ * @Author: zhangping
+ * @Date: 2019-06-11 20:08:52
+ * @LastEditTime: 2019-06-25 11:56:44
+ * @LastEditors: Please set LastEditors
  */
-import Home from '../pages/home/Index'
-import Mine from '../pages/mine/Index'
-import List from '../pages/list/Index'
+import { lazy } from 'react'
 
 /**
- * 获取组件
- * @param {String} file 文件路径
+ * 路由懒加载
+ * @param {String} filename 文件路径
  */
+ const lazyRouter = (filename: string) => {
+  return lazy(() => import(`../pages/${filename}`))
+ }
 
 /**
- * path-访问的路径
- * name-名称
- * component-应用的组件
- * needLogin-是否需要登录
- * redirect-重定向
- * auth-能访问该路由的权限（没有就证明没有权限限制）
+ * path: string-访问的路径
+ * name: string-名称
+ * component: ReactElement-应用的组件
+ * needLogin: boolean-是否需要登录
+ * redirect: string-重定向
+ * auth: Array-能访问该路由的权限（没有就证明没有权限限制），auth: ['admin', 'user']
  */
 const RouterMap = [
-  { path: '/', name: '首页', component: Home, needLogin: false, redirect: null, auth: [] },
-  { path: '/mine', name: '我的', component: Mine, needLogin: false, redirect: null, auth: [] },
-  { path: '/list', name: '列表', component: List, needLogin: false, redirect: null, auth: [] },
+  { path: '/', name: '首页', component: lazyRouter('home/Index') },
+  { path: '/mine', name: '我的', component: lazyRouter('mine/Index') },
+  { path: '/list', name: '列表', component: lazyRouter('list/Index') },
 ]
 
 export default RouterMap
